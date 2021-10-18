4213576406920aa0a80fd755a6961204c0d2c44c
youssif hany <youssifhany75@gmail.com>
2021-10-18 17:27:20 +0200

    update counter-style

--- src/components/CounterContainer/CounterAction/CounterAction.jsx
+++ src/components/CounterContainer/CounterAction/CounterAction.jsx
@@ -1,13 +1,22 @@
+import classes from "./CounterAction.module.css";
+
 const CounterAction = (props) => {
   return (
     <>
       <button
+        className={`${classes.btn} ${classes["btn-increase"]}`}
         onClick={() => props.onChangeCounter((prevCounter) => ++prevCounter)}
       >
         Increase
       </button>
       <button
-        onClick={() => props.onChangeCounter((prevCounter) => --prevCounter)}
+        className={`${classes.btn} ${classes["btn-decrease"]}`}
+        onClick={() =>
+          props.onChangeCounter((prevCounter) => {
+            if (prevCounter) return --prevCounter;
+            else return 0;
+          })
+        }
       >
         Decrease
       </button>
